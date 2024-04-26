bcdc810ca0f8af6f2e4b30e62aff5dacb03b0b30
KahfiSmith <alkahfii2018@gmail.com>
2024-04-26 23:26:26 +0700

    done navbar, nama, about me

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: 'rgb(15, 23, 42)',
-        primary: 'rgb(30, 41, 59)',
-        secondary: 'rgb(43, 55, 73)',
-        icon: 'rgb(45, 212, 194)',
-        color: 'rgb(203, 213, 225)',
-        border: 'rgb(76, 90, 110)',
-        'bg-tag': 'rgb(25, 51, 64)',
-        'bg-hover': 'rgb(22, 32, 50)'
-      }
+        background: "rgb(15, 23, 42)",
+        primary: "rgb(30, 41, 59)",
+        secondary: "rgb(43, 55, 73)",
+        icon: "rgb(45, 212, 194)",
+        color: "rgb(203, 213, 225)",
+        border: "rgb(76, 90, 110)",
+        "bg-tag": "rgb(25, 51, 64)",
+        "bg-hover": "rgb(22, 32, 50)",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        lora: ["Lora", "sans-serif"],
+      },
     },
   },
   plugins: [],
