a2162eee1d0377ad80d6cf25c841bc364566a616
KahfiSmith <alkahfii2018@gmail.com>
2024-05-03 09:21:30 +0700

    feat: change the relative path for html file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./index.html", "./public/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
